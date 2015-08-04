f10a4c6fbc252d38eed8b1a0c1918959d8983129
HARADA REO <rharada@golyat.local>
2015-08-05 01:49:07 +0900

    Initial Commit

---
+++ 紙芝居/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  紙芝居
+//
+//  Created by HARADA REO on 2015/08/05.
+//  Copyright (c) 2015年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
